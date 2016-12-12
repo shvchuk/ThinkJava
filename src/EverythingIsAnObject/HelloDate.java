efd38af7e1d2028d30fe1b4b96e6c463f93837da
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2016-12-12 21:07:53 +0100

    Initial commit

---
+++ src/EverythingIsAnObject/HelloDate.java
@@ -0,0 +1,11 @@
+package EverythingIsAnObject;
+
+import java.util.Date;
+
+public class HelloDate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, its: ");
+        System.out.println(new Date());
+    }
+}
---
+++ src/Operators/HelloDate.java
@@ -0,0 +1,10 @@
+package Operators;
+
+import java.util.Date;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, its: ");
+        System.out.println(new Date());
+    }
+}
